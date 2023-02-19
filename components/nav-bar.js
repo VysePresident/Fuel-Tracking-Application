990e3e5f38066f7e9b6059510058097b148ac240
Liang Yu Lin <tanyalin2001@gmail.com>
2023-02-19 01:52:23 -0600

    Basic setup of index.html and navbar/footer

---
+++ components/nav-bar.js
@@ -0,0 +1,33 @@
+// get the nav-bar element
+const navBar = document.getElementById('nav-bar');
+
+// set the HTML for the navigation bar
+navBar.innerHTML = `
+  <header>
+    <a href="./index.html"><img src="img/logo.png"></a>
+    <nav>
+      <i class="fa fa-bars" onclick="showMenu()"></i>
+      <div class="nav-links" id="navLinks">
+        <i class="fa fa-times" onclick="hideMenu()"></i>
+        <ul>
+          <li><a href="contact.html">CONTACT US</a></li>
+          <li><a href="about.html">ABOUT</a></li>
+          <li><a href="register.html">REGISTER</a></li>
+          <li><a href="signin.html">SIGN IN</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+`;
+
+// get the navLinks element
+const navLinks = document.getElementById("navLinks");
+
+// show/hide the navigation menu when the user clicks the menu icon
+function showMenu() {
+  navLinks.style.right = "0";
+}
+
+function hideMenu() {
+  navLinks.style.right = "-200px";
+}
