9af83bfe4e05dc6ebe5add0b5f5a736359871539
Liang Yu Lin <tanyalin2001@gmail.com>
2023-02-24 05:37:05 -0600

    Add blur, adjust brigtness, bg pic to userProfile

--- components/userProfile.js
+++ components/userProfile.js
@@ -1,79 +1,75 @@
-const home = document.getElementById('userProfile');
-const container = document.createElement('div');
+const home = document.getElementById("userProfile");
+const container = document.createElement("div");
 
-container.className = 'container';
+container.className = "container";
 container.innerHTML = `
 <body>
     <form class="formTable" action="orderSummary.php" method="POST">
         <div class="nav-bar" id="nav-bar"></div>
-        <section>
-            <h1>PROFILE</h1>
+        <section id="formbox">
+            <h1>USER PROFILE</h1>
 
             <section>
-              <h2 class="formHeader">PROFILE INFORMATION</h2>
-              <p class="formRow">
-                  <label class="formCell" for="custEmail">Email:</label>
+              <h2 class=>Profile Information</h2>
+              <div class="formRow">
+                  <p class="formCell" for="custEmail">Email:</p>
                   <input class="formCell" type="text" name="custEmail" id="custEmail" required>
-              </p>
-              <p class="formRow">
-                  <label class="formCell" for="password">Password:</label>
+              </div>
+              <div class="formRow">
+                  <p class="formCell" for="password">Password:</p>
                   <input class="formCell" type="password" name="password" id="password" minlength="8" required>
-              </p>
-              <p class="formRow">
-                <label class="formCell" for="password">Confirm Password:</label>
+              </div>
+              <div class="formRow">
+                <p class="formCell" for="password">Confirm Password:</p>
                 <input class="formCell" type="password" name="password" id="password" minlength="8" required>
-              </p>
+              </div>
             </section>
   
 
             <section>
-              <h2 class="formHeader">PERSONAL INFORMATION</h2>
-              <p class="formRow">
-                  <label class="formCell" for="fname">First Name:</label>
+              <h2 class="formHeader">Personal Information</h2>
+              <div class="formRow">
+                  <p class="formCell" for="fname">First Name:</p>
                   <input class="formCell" type="text" name="fname" id="fname" maxlength="25" required>
-              </p>
-              <p class="formRow">
-                <label class="formCell" for="mname">Middle Name (optional):</label>
+              </div>
+              <div class="formRow">
+                <p class="formCell" for="mname">Middle Name (optional):</p>
                 <input class="formCell" type="text" name="mname" id="mname" maxlength="25">
-            </p>
-              <p class="formRow">
-                  <label class="formCell" for="lname">Last Name:</label>
+              </div>
+              <div class="formRow">
+                  <p class="formCell" for="lname">Last Name:</p>
                   <input class="formCell" type="text" name="lname" id="lname" maxlength="25" required>
-              </p class="formRow">
-              <p class="formRow">
-                  <label class="formCell" for="custEmail">Email:</label>
-                  <input class="formCell" type="text" name="custEmail" id="custEmail" required>
-              </p>
-              <p class="formRow">
-                  <label class="formCell" for="phone">Phone Number:</label>
+              </div class="formRow">
+              <div class="formRow">
+                  <p class="formCell" for="phone">Phone Number:</p>
                   <input class="formCell" type="tel" name="phone" id="phone" required>
-              </p>
+              </div>
             </section>
   
-            <h2>COMPANY INFORMATION</h2>
-            <p class="formRow">
-                <label class="formCell" for="companyName">Company Name:</label>
+            <h2>Company Information</h2>
+            <div class="formRow">
+                <p class="formCell" for="companyName">Company Name:</p>
                 <input class="formCell" type="text" name="companyName" id="companyName" required>
-            </p>
+            </div>
             
-            <p class="formRow">
-                <label for="state" class="formCell">State:</label>
+            <div class="formRow">
+                <p for="state" class="formCell">State:</p>
                 <select class="formCell" name="state" id="state" required>
-                    <script src="components/states.js"></script>
+                    <option src="components/states.js"></option>
                 </select>
-            </p>
-            <p class="formRow">
-                <label class="formCell" for="city">City:</label>
+            </div>
+            <div class="formRow">
+                <p class="formCell" for="city">City:</p>
                 <input class="formCell" type="text" name="city" id="city" maxlength="100" required>
-            </p>
-            <p class="formRow">
-                <label class="formCell" for="street">Street Address:</label>
+            </div>
+            <div class="formRow">
+                <p class="formCell" for="street">Street Address:</p>
                 <input class="formCell" type="text" name="street" id="street" maxlength="100" required>
-            </p>
-            <p class="formRow">
-              <label class="formCell" for="zipcode">Zip Code:</label>
+            </div>
+            <div class="formRow">
+              <p class="formCell" for="zipcode">Zip Code:</p>
               <input class="formCell" type="text" name="zipcode" id="zipcode" maxlength="9" required>
-          </p>
+            </div>
         </section>
 
         <section>
@@ -84,3 +80,14 @@ container.innerHTML = `
 </body>
 `;
 home.appendChild(container);
+
+// Get the select element
+const stateSelect = document.getElementById("state");
+
+// Add the states to the select element
+for (let i = 0; i < states.length; i++) {
+  const option = document.createElement("option");
+  option.value = states[i].code;
+  option.textContent = states[i].name;
+  stateSelect.appendChild(option);
+}
