101ffe5f6087f6979f5bca75252bea2c3a30545a
Liang Yu Lin <tanyalin2001@gmail.com>
2023-03-19 19:05:22 -0500

    Add validation functionality

--- server/userProfile.php
+++ server/userProfile.php
@@ -37,23 +37,89 @@ $street = clean_input($_POST['street']);
 $street2 = clean_input($_POST['street2']);
 $zipcode = clean_input($_POST['zipcode']);
 
-// Hash the password
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+function validate_input($data) {
+    // Check for required fields
+    $required_fields = ['custEmail', 'password', 'fname', 'lname', 'phone', 'companyName', 'state', 'city', 'street', 'zipcode'];
+    foreach ($required_fields as $field) {
+        if (!isset($data[$field]) || empty($data[$field])) {
+            return false;
+        }
+    }
 
-// Prepare and execute SQL statement
-$sql = "INSERT INTO user_profiles (email, password, fname, mname, lname, phone, companyName, state, city, street, street2, zipcode)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    // Check email format
+    if (!filter_var($data['custEmail'], FILTER_VALIDATE_EMAIL)) {
+        return false;
+    }
 
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("ssssssssssss", $email, $hashed_password, $fname, $mname, $lname, $phone, $companyName, $state, $city, $street, $street2, $zipcode);
+    // Check password length
+    if (strlen($data['password']) < 8) {
+        return false;
+    }
+
+    // Check phone number format
+    if (!preg_match('/^\d{10,20}$/', $data['phone'])) {
+        return false;
+    }
+
+    // Check zipcode format
+    if (!preg_match('/^\d{5}(-\d{4})?$/', $data['zipcode'])) {
+        return false;
+    }
+
+    // Validate other string lengths according to the database schema
+    $string_lengths = [
+        'fname' => 50,
+        'mname' => 50,
+        'lname' => 50,
+        'companyName' => 100,
+        'city' => 100,
+        'street' => 100,
+        'street2' => 100
+    ];
+    foreach ($string_lengths as $field => $length) {
+        if (isset($data[$field]) && strlen($data[$field]) > $length) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+if (validate_input([
+    'custEmail' => $email,
+    'password' => $password,
+    'fname' => $fname,
+    'mname' => $mname,
+    'lname' => $lname,
+    'phone' => $phone,
+    'companyName' => $companyName,
+    'state' => $state,
+    'city' => $city,
+    'street' => $street,
+    'street2' => $street2,
+    'zipcode' => $zipcode
+])) {
+    // Hash the password
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    // Prepare and execute SQL statement
+    $sql = "INSERT INTO user_profiles (email, password, fname, mname, lname, phone, companyName, state, city, street, street2, zipcode)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssssssssssss", $email, $hashed_password, $fname, $mname, $lname, $phone, $companyName, $state, $city, $street, $street2, $zipcode);
+} else {
+    echo "Invalid input data";
+}
 
 if ($stmt->execute()) {
     echo "New record created successfully";
-    // Redirect to a confirmation page
-    // header("Location: confirmed_profile.html");
+    //Redirect to a confirmation page
+    header("Location: ../confirmed_profile.html");
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
 
 $stmt->close();
 $conn->close();
