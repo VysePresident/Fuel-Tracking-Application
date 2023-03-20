3f2daca6c656ffaa6f2218974c4eaec7cc34292c
Liang Yu Lin <tanyalin2001@gmail.com>
2023-03-20 00:58:13 -0500

    Basic setup of composer

---
+++ src/userProfileManager.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class UserProfileManager {
+    public function validate_input($data) {
+        //  validation logic 
+    }
+
+    // Other functions that we want to test
+}
