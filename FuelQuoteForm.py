47297d0524a4b7e4659dc5664250a39970c509f5
Jason Mullen <75854582+JasonMullen@users.noreply.github.com>
2023-03-22 14:35:28 -0500

    Update FuelQuoteForm.py
    i'll convert code to python tonight or tomorrow

--- FuelQuoteForm.py
+++ FuelQuoteForm.py
@@ -1,4 +1,4 @@
-#Fuel Quote Class
+#FUEL QUOTE FORM CLASS
 import sqlite3
 import re
 
@@ -34,6 +34,18 @@ class FuelQuote:
             cursor.execute('''SELECT * FROM fuel_quotes''')
             return cursor.fetchall()
 
+    def get_last_quote_location(self):
+        with sqlite3.connect(self.db_name) as connection:
+            cursor = connection.cursor()
+            cursor.execute('''SELECT city, state FROM fuel_quotes ORDER BY id DESC LIMIT 1''')
+            return cursor.fetchone()
+
+    def get_num_quotes(self):
+      with sqlite3.connect(self.db_name) as connection:
+          cursor = connection.cursor()
+          cursor.execute('''SELECT COUNT(*) FROM fuel_quotes''')
+          return cursor.fetchone()[0]
+          
     # Validate state input
     def is_valid_state(self, state):
         state_pattern = r'^[A-Za-z]{2}$'
@@ -81,9 +93,8 @@ if __name__ == '__main__':
     print("All fuel quotes in the database:")
     for quote in fuel_quote.get_all_quotes():
         print(quote)
-        
-        
-#Order class
+
+#ORDER CLASS
 import sqlite3
 import re
 
@@ -199,49 +210,199 @@ if __name__ == '__main__':
     for o in order.get_all_orders():
         print(o)
     
-#Trucking class 
-!pip install geopy
-
-import requests
+#TRUCKING AND TRUCKING COST CLASS
+import sqlite3
+import math 
+import re
 from geopy import distance
 from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut, GeocoderServiceError
 
+# Order and FuelQuote classes remain the same as before
+
+# The Trucking class calculates the distance between two locations and the cost of transporting fuel.
 class Trucking:
-    def __init__(self, user_agent):
+    def __init__(self, user_agent, starting_location):
         self.user_agent = user_agent
         self.geolocator = Nominatim(user_agent=self.user_agent)
-        self.starting_location = "University of Houston, Houston, TX"
-
+        self.starting_location = starting_location
+    # This method calculates the distance in miles between the starting location and destination address.
     def get_distance(self, destination_address):
-        starting_point = self.geolocator.geocode(self.starting_location)
-        destination_point = self.geolocator.geocode(destination_address)
-
-        if starting_point and destination_point:
-            return distance.distance((starting_point.latitude, starting_point.longitude),
-                                      (destination_point.latitude, destination_point.longitude)).miles
-        else:
+        try:
+            starting_location = self.geolocator.geocode(self.starting_location, addressdetails=True, timeout=10)
+            destination_location = self.geolocator.geocode(destination_address, addressdetails=True, timeout=10)
+        except (GeocoderTimedOut, GeocoderServiceError):
             return None
 
-    def is_valid_address(self, address):
-        geocode_result = self.geolocator.geocode(address)
-        return geocode_result is not None
+        if starting_location and destination_location:
+            starting_point = (starting_location.raw['lat'], starting_location.raw['lon'])
+            destination_point = (destination_location.raw['lat'], destination_location.raw['lon'])
+            return distance.distance(starting_point, destination_point).miles
+        else:
+            return None
+    # This method calculates the final cost of transporting fuel based on the order and fuel quote information.
+    def calculate_final_cost(self, order, fuel_quote):
+        # Constants
+        MAX_CAPACITY = 3000
+        PRICE_PER_GALLON = 3.022
+        MPG = 6.5
+        # Get the last order and quote from the order and fuel_quote instances
+        gallons = order.get_all_orders()[-1][2]
+        city, state = fuel_quote.get_last_quote_location()
+        destination_address = f"{city}, {state}"
+
+        if gallons <= 0:
+            return None
+
+        distance_in_miles = self.get_distance(destination_address)
+
+        if distance_in_miles is None:
+            return None
+
+        distance_cost = distance_in_miles / MPG
+
+        # Calculate the number of trucks needed
+        num_trucks = math.ceil(gallons / MAX_CAPACITY)
+        remaining_gallons = gallons
+
+        # Calculate the final cost based on the number of trucks
+        final_cost = 0
+        for _ in range(num_trucks):
+            if remaining_gallons > MAX_CAPACITY:
+                gallons_on_truck = MAX_CAPACITY
+            else:
+                gallons_on_truck = remaining_gallons
+
+            cost_for_truck = gallons_on_truck * PRICE_PER_GALLON + distance_cost
+            final_cost += cost_for_truck
+            remaining_gallons -= MAX_CAPACITY
+
+        return final_cost
+
+# The TruckingCost class handles the database operations related to storing and retrieving trucking costs.
+class TruckingCost:
+    def __init__(self, db_name="trucking_costs.db"):
+        self.db_name = db_name
+        self.create_table()
+    # This method creates the trucking_costs table if it doesn't exist.
+    def create_table(self):
+        with sqlite3.connect(self.db_name) as connection:
+            cursor = connection.cursor()
+            cursor.execute('''CREATE TABLE IF NOT EXISTS trucking_costs
+                              (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                              order_id INTEGER NOT NULL,
+                              fuel_quote_id INTEGER NOT NULL,
+                              final_cost REAL NOT NULL)''')
+
+    def add_cost(self, order_id, fuel_quote_id, final_cost):
+        with sqlite3.connect(self.db_name) as connection:
+            cursor = connection.cursor()
+            cursor.execute('''INSERT INTO trucking_costs (order_id, fuel_quote_id, final_cost)
+                              VALUES (?, ?, ?)''', (order_id, fuel_quote_id, final_cost))
+            connection.commit()
+
+    def get_all_costs(self):
+        with sqlite3.connect(self.db_name) as connection:
+            cursor = connection.cursor()
+            cursor.execute('''SELECT * FROM trucking_costs''')
+            return cursor.fetchall()
 
 if __name__ == "__main__":
-    # Replace "YourAppName" with your actual application name
-    user_agent = "YourAppName/1.0"
-    trucking = Trucking(user_agent)
+  # Replace "YourAppName" with your actual application name
+  user_agent = "YourAppName/1.0"
+  DEFAULT_STARTING_LOCATION = "University of Houston, Houston, TX"
+  
+  # Set the starting location to the default starting location (University of Houston, Houston, TX)
+  starting_location = DEFAULT_STARTING_LOCATION
 
-    while True:
-        destination_address = input("Enter the destination address: ")
+  # Create instances of Order, FuelQuote, Trucking, and Truck
+  trucking = Trucking(user_agent, starting_location)
+  # Create instances of the Order and FuelQuote classes
+  order = Order()
+  fuel_quote = FuelQuote()
+  # Add your order and fuel quote information here
+  # ...
 
-        if trucking.is_valid_address(destination_address):
-            break
-        else:
-            print("Invalid address. Please enter a valid address.")
+  # Get the city and state from the last added fuel quote
+  city, state = fuel_quote.get_last_quote_location()
 
-    distance_in_miles = trucking.get_distance(destination_address)
+  # Create an instance of the Trucking class
+  trucking = Trucking(user_agent, starting_location)
 
-    if distance_in_miles:
-        print(f"The distance from the University of Houston to the destination is {distance_in_miles:.2f} miles.")
-    else:
-        print("Failed to calculate the distance. Please check the input address.")
+  # Set the destination address based on the city and state from the FuelQuote class
+  destination_address = f"{city}, {state}"
+
+  distance_in_miles = trucking.get_distance(destination_address)
+  if distance_in_miles:
+      print(f"The distance from {trucking.starting_location} to {destination_address} is {distance_in_miles:.2f} miles.")
+  else:
+      print("Failed to calculate the distance. Please check the input addresses.")
+
+  # Add an Order to the database (assuming the user has provided valid input)
+  order.add_order(fuel_type, gallons, first_name, last_name, email, phone_number, payment_type)
+
+  final_cost = trucking.calculate_final_cost(order, fuel_quote)
+
+  if final_cost:
+      print(f"The final cost for transporting the fuel is ${final_cost:.2f}.")
+  else:
+      print("Failed to calculate the final cost. Please check the input addresses.")
+
+ #FINAL REPORT
+import sqlite3
+
+class FinalReport:
+    def __init__(self, db_name='final_reports.db'):
+        self.db_name = db_name
+        self.create_table()
+
+    def create_table(self):
+        with sqlite3.connect(self.db_name) as connection:
+            cursor = connection.cursor()
+            cursor.execute('''CREATE TABLE IF NOT EXISTS final_reports
+                              (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                              company_name TEXT NOT NULL,
+                              state TEXT NOT NULL,
+                              city TEXT NOT NULL,
+                              address TEXT NOT NULL,
+                              fuel_type TEXT NOT NULL,
+                              gallons INTEGER NOT NULL,
+                              first_name TEXT NOT NULL,
+                              last_name TEXT NOT NULL,
+                              email TEXT NOT NULL,
+                              phone_number TEXT NOT NULL,
+                              payment_method TEXT NOT NULL,
+                              distance REAL NOT NULL,
+                              final_cost REAL NOT NULL)''')
+
+    def add_report(self, order, fuel_quote, trucking, final_cost):
+        # Extract the required information from the instances of the other classes
+        last_order = order.get_all_orders()[-1]
+        fuel_type, gallons, first_name, last_name, email, phone_number, payment_method = last_order[1:]
+
+        last_quote = fuel_quote.get_all_quotes()[-1]
+        company_name, state, city, address = last_quote[1:]
+
+        distance = trucking.get_distance(f"{city}, {state}")
+
+        with sqlite3.connect(self.db_name) as connection:
+            cursor = connection.cursor()
+            cursor.execute('''INSERT INTO final_reports (company_name, state, city, address, fuel_type, gallons, first_name, last_name, email, phone_number, payment_method, distance, final_cost)
+                              VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', (company_name, state, city, address, fuel_type, gallons, first_name, last_name, email, phone_number, payment_method, distance, final_cost))
+            connection.commit()
+
+    def get_all_reports(self):
+        with sqlite3.connect(self.db_name) as connection:
+            cursor = connection.cursor()
+            cursor.execute('''SELECT * FROM final_reports''')
+            return cursor.fetchall()
+if __name__ == "__main__":
+    # ... (previous code)
+    # Store the final report in the database
+    
+    final_report = FinalReport()
+    final_report.add_report(order, fuel_quote, trucking, final_cost)
+
+    print("All orders in the database:")
+    for report in final_report.get_all_reports():
+        print(report)
