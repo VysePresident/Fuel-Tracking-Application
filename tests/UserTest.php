bd308b0c50ac9b4cbe3a84ef9c24e6de01821b9c
Liang Yu Lin <tanyalin2001@gmail.com>
2023-03-24 04:28:11 -0500

    Passed unit tests for verification, sanilization and createUser

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -4,23 +4,61 @@ use App\User;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase{
-    private $user;
+    private $connection;
 
-    protected function setUp(): void{
-        $mysqli = $this->createMock(\mysqli::class);
-        $this->user = new User($mysqli);
+    protected function setUp(): void
+    {
+        $servername = "localhost";
+        $username = "root";
+        $password = "GasCo12345678";
+        $dbname = "gasco"; // Use a test version of your database
+        $port = 3307;
+
+        $this->connection = new mysqli($servername, $username, $password, $dbname, $port);
     }
-
+    protected function tearDown(): void
+    {
+        $this->connection->close();
+    }
+    /**
+     * @covers \App\User::clean_input
+     */
     public function testCleanInput(){
+        $user = new User($this->connection);
         $dirtyInput = "    <script>alert('test');</script> \t";
         $expectedOutput = "&lt;script&gt;alert(&#039;test&#039;);&lt;/script&gt;";
 
-        $cleanedInput = $this->user->clean_input($dirtyInput);
+        $cleanedInput = $user->clean_input($dirtyInput);
         $this->assertEquals($expectedOutput, $cleanedInput);
     }
-
-    public function testValidateInput()
+    /**
+    * @covers \App\User::createUser
+    */
+    public function testCreateUser()
     {
+        $user = new User($this->connection);
+
+        $validData = [
+            'custEmail' => 'test@example.com',
+            'password' => 'test1234',
+            'fname' => 'John',
+            'lname' => 'Doe',
+            'phone' => '1234567890',
+            'companyName' => 'Test Company',
+            'state' => 'NY',
+            'city' => 'New York',
+            'street' => '123 Main St',
+            'zipcode' => '12345'
+        ];
+
+        $this->assertTrue($user->createUser($validData));
+    }
+
+    /**
+    * @covers \App\User::validate_input
+    */
+    public function testValidateInput(){
+        $user = new User($this->connection);
         $validData = [
             'custEmail' => 'test@example.com',
             'password' => 'password123',
@@ -37,52 +75,7 @@ class UserTest extends TestCase{
         $invalidData = $validData;
         $invalidData['custEmail'] = 'invalid_email';
 
-        $this->assertTrue($this->user->validate_input($validData));
-        $this->assertFalse($this->user->validate_input($invalidData));
+        $this->assertTrue($user->validate_input($validData));
+        $this->assertFalse($user->validate_input($invalidData));
     }
-
-    public function testCreateUser() {
-        $validData = [
-            'custEmail' => 'test@example.com',
-            'password' => 'password123',
-            'fname' => 'John',
-            'lname' => 'Doe',
-            'phone' => '1234567890',
-            'companyName' => 'Test Company',
-            'state' => 'Texas',
-            'city' => 'Missouri City',
-            'street' => '123 Main Street',
-            'zipcode' => '90001'
-        ];
-
-        $invalidData = $validData;
-        $invalidData['custEmail'] = 'invalid_email';
-
-        $this->assertTrue($this->user->createUser($validData));
-        $this->assertFalse($this->user->createUser($invalidData));
-    }
-    public function testUpdateUser() {
-        $validData = [
-            'custEmail' => 'test@example.com',
-            'password' => 'password123',
-            'fname' => 'John',
-            'lname' => 'Doe',
-            'phone' => '1234567890',
-            'companyName' => 'Test Company',
-            'state' => 'Texas',
-            'city' => 'Missouri City',
-            'street' => '123 Main Street',
-            'zipcode' => '90001'
-        ];
-    
-        $userId = 1;
-    
-        $this->assertTrue($this->user->updateUser($validData, $userId));
-    
-        $invalidData = $validData;
-        $invalidData['custEmail'] = 'invalid_email';
-    
-        $this->assertFalse($this->user->updateUser($invalidData, $userId));
-    }
-    
 }
