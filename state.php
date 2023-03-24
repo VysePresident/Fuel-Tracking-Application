e3e50d4b54f9598a31772367418550b033eabbd2
BuildTools <AgentIllusion73@gmail.com>
2023-03-24 00:03:27 -0500

    Removed some HTML from PHP pages

--- state.php
+++ state.php
@@ -8,8 +8,6 @@
     <link rel="stylesheet" href="styles/styles.css">
     <link rel="stylesheet" href="styles/orderSummary.css">
 
-    <title>Order Summary</title>
-
     <script src="components/nav-bar.js" defer></script>
     <script src="components/footer.js" defer></script>
     <script src="components/orderSummary.js" defer></script>
@@ -45,34 +43,4 @@
         }
     ?>
 </head>
-
-<body>
-    <form class="orderSummary" action="orderConfirmation.html">
-    <div class="nav-bar" id="nav-bar"></div>
-        <section name="summarySection">
-            <h1><u>ORDER SUMMARY</u></h1>
-            <p>Confirm your order:</p>
-                <?php echo "<p>Ship to $companyName at: $street $city, $state</p>" ?>
-                <?php echo "<p>$gallonsRequested gallons of $fuelType</p>" ?>
-                <?php echo "<p>Deliver By: $deliveryDate</p>" ?>
-        </section>
-            <div class="priceEstimate" name="priceEstimate", id="priceEstimate">
-                <ul class="priceEstimate">
-                    <li class="priceEstimate"><?php echo "<p>Price/Gallon: $pricePerGallon</p>" ?></li>
-                    <li class="priceEstimate"><?php echo "<p>Total Price: $totalPrice</p>" ?></li>
-                </ul>
-            </div>
-        </section>
-        <section class="buttons">
-            <input type="submit" value="Confirm" class="confirmButton" id="confirmButton" onclick="confirmOrder()">
-            <input type="submit" formaction="index.html" value="Cancel" class="cancelButton" id="cancelButton" onclick="goHome()">
-        </section>
-    </form>
-</body>
-
-<footer>
-    <section>
-        <div class="footer" id="footer"></div>
-    </section>
-</footer>
 </html>
