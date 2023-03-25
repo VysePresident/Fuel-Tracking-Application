4dc42b9ae2af89f61d2e8eda058cc9fd016a3006
VysePresident <imgenius77x7@gmail.com>
2023-03-24 22:38:16 -0500

    Improved unit testing

--- tests/hcUserTest.php
+++ tests/hcUserTest.php
@@ -1,14 +1,23 @@
 <?php
-require_once __DIR__ . '/../server/hcUser.php';
+require_once __DIR__ . '/../src/hcUser.php';
+//require_once '../src/hcUser.php';
 
 class hcUserTest extends PHPUnit\Framework\TestCase
 {
+    /**
+     * @covers hcUser::getEmail
+     * @covers \App\User::__construct
+     */
     public function testGetEmail()
     {
         $user = new hcUser('test@example.com', 'John', 'Doe', '123-456-7890', 'password');
         $this->assertEquals('test@example.com', $user->getEmail());
     }
 
+    /**
+     * @covers hcUser::setEmail
+     * @covers \App\User::__construct
+     */
     public function testSetEmail()
     {
         $user = new hcUser('test@example.com', 'John', 'Doe', '123-456-7890', 'password');
