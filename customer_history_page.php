b27e94464024c2365282a3404e82ed32f1cafe4f
BuildTools <AgentIllusion73@gmail.com>
2023-03-31 22:52:45 -0500

    Update customer_history_page.php

--- customer_history_page.php
+++ customer_history_page.php
@@ -1,39 +1,30 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Order History</title>
-        <!--Linking the page that allows one to put restrictions on the email input fields-->
-        <script src="email.validation.js"></script>
-        <!--Linking the page that allows one to put restrictions on the password input fields-->
-        <script src="password.validation.js"></script>
-        <!--This is section of the code is primarily for the top of the page. 
-        For ex the image, the nav bar, and the links.-->
-        <meta charset = "UTF-8" />
-        <meta name = "viewport" content  = "width=device-width, initial-scale = 1.0" />
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
-        <link rel = "stylesheet" href = "Customer_history_page.css">
-      <header>
-        <a href="./index.html"><img src="logo.png"></a>
-        <link rel="stylesheet" href="nav-bar.css">
-        <nav>
-            <i class="fa fa-bars" onclick="showMenu()"></i >
-            <div class="nav-links" id="navLinks">
-              <i class="fa fa-times" onclick="hideMenu()"></i>
-              <ul>
-                <li><a href="contact.html">CONTACT US</a></li>
-                <li><a href="about.html">ABOUT</a></li>
-                <li><a href="register.html">REGISTER</a></li>
-                <li><a href="Login_Page.html">SIGN IN</a></li>
-                <li><a></a></li>
-              </ul>
-            </div>
-        </nav>
-    </header>
+      
+      <title>Order History</title>
+      <!--Linking the page that allows one to put restrictions on the email input fields-->
+      <script src="email.validation.js"></script>
+      <!--Linking the page that allows one to put restrictions on the password input fields-->
+      <script src="password.validation.js"></script>
+      <!--This is section of the code is primarily for the top of the page. 
+      For ex the image, the nav bar, and the links.-->
+      <meta charset = "UTF-8" />
+      <meta name = "viewport" content  = "width=device-width, initial-scale = 1.0" />
+      <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
+      <link rel = "stylesheet" href = "Customer_history_page.css">
+      <?php
+        include_once 'components/nav-bar.php';
+        ini_set("display_errors", "1");
+        ini_set("display_startup_errors", "1");
+        error_reporting(E_ALL);
+      ?>
     </head>
   <?php
-    $email = "tom@example.com";
+    $client = $_SESSION['client'];
+    $email = $client->getEmail();
   ?>
-    <table id="orderHistoryTable">
+    <table id="orderHistoryTable" style="padding-top: 20px;">
       <thead>
         <tr>
           <th>Order ID</th>
@@ -46,9 +37,7 @@
       </thead>
       <tbody>
         <?php
-          ini_set("display_errors", "1");
-          ini_set("display_startup_errors", "1");
-          error_reporting(E_ALL);
+
           include("src/connection.php");
 
           
@@ -73,6 +62,15 @@
     </table>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+    <!--This keeps the table below the nav bar-->
+    <style>
+      .dataTables_length {
+        margin-top: 120px;
+      }
+      .dataTables_filter {
+        margin-top: 120px;
+      }
+    </style>
     <script>
       $(document).ready(function() {
         $('#orderHistoryTable').DataTable({
@@ -81,15 +79,10 @@
       });
 
     </script>
-    <footer>
-		<!--This is where the footer of the page will be-->
-		<section>
-			<div class="footer" id="footer"></div>
-		</section>
-		<link rel="stylesheet" href="footer.css">
-		<div class="footer" id="footer"></div>
-		<script src="nav-bar.js"></script>
-		<script src="footer.js"></script>
-    </footer>
-  </body>
+</body>
+<footer>
+    <div id="footerWrapper">
+      <div class="footer" id="footer"></div>
+    </div>
+</footer>
 </html>
