44524a4d642db674f4c1166eac6eca415ad79c24
VysePresident <imgenius77x7@gmail.com>
2023-04-20 04:19:39 -0500

    Completed Pricing Module & set up Quote Button
    Completed Pricing Module & set up Quote Button.
    
    Bulk of work should be done, just need to disable the buttons when fields aren't filled & update the orderSummary.php page to use the correct pricing formula.

--- components/fuelQuoteForm.js
+++ components/fuelQuoteForm.js
@@ -1,1 +1,72 @@
-//Currently None
+// Get Quote Button Click Event
+document.getElementById("getQuoteBtn").addEventListener("click", function (event) {
+    event.preventDefault();
+    // TESTING TESTING TESTING
+    console.log("This is 1");
+  
+    // Get form values
+    var fuelType = document.getElementById("fuelType").value;
+    var gallonsRequested = document.getElementById("gallonsRequested").value;
+    var deliveryDate = document.getElementById("deliveryDate").value;
+  
+    // Check if all fields are filled
+    if (
+      fuelType !== "" &&
+      gallonsRequested !== "" &&
+      deliveryDate !== ""
+    ) {
+        // TESTING TESTING TESTING
+        console.log("This is 2");
+
+      // Disable Get Quote and Submit Quote buttons
+      //document.getElementById("getQuoteBtn").disabled = true;
+      //document.getElementById("submitBtn").disabled = true;
+  
+      // Create data object to send via AJAX
+      var data = {
+        fuelType: fuelType,
+        gallonsRequested: gallonsRequested,
+        deliveryDate: deliveryDate,
+      };
+
+      // TESTING TESTING TESTING
+      console.log("This is 3");
+  
+      // Make AJAX call to Pricing Module
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "server/pricingModule.php", true);
+      xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+      // TESTING TESTING TESTING
+      console.log("This is 4");
+
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+
+            // TESTING TESTING TESTING
+            console.log("This is 5");
+
+          console.log(xhr.responseText) // TESTING TESTING TESTING
+          // Parse response JSON
+          var response = JSON.parse(xhr.responseText);
+  
+          // Populate Suggested Price and Total Amount fields
+          document.getElementById("suggestedPrice").value = response.suggestedPrice;
+          document.getElementById("totalAmount").value = response.totalAmount;
+
+          // TESTING TESTING TESTING
+            console.log("This is 6");
+  
+          // Enable Submit Quote button
+          //document.getElementById("submitBtn").disabled = false;
+          console.log(response); // TESTING TESTING TESTING
+        }
+      };
+      xhr.send(JSON.stringify(data));
+    } else {
+      // Show error message if any field is empty
+      // TESTING TESTING TESTING
+      console.log("This is error#");
+      alert("Please fill in all the required fields.");
+    }
+  });
---
+++ server/pricingModule.php
@@ -0,0 +1,78 @@
+    <?php
+
+
+    ini_set("display_errors", "1");
+    ini_set("display_startup_errors", "1");
+    error_reporting(E_ALL);
+    //include_once 'server/loginSession.php'; // Maintains session information
+
+    require_once __DIR__ . '/db_connection.php';
+    require_once __DIR__ . '/../src/client.php';
+    //require_once __DIR__ . '/../components/nav-bar.php';
+
+    session_start();
+
+    $client = $_SESSION['client'];
+
+    // Constants
+
+    $currentPricePerGallon = 1.50; // Current price per gallon
+
+    $locationFactorTexas = 0.02; // Location factor for Texas
+    $locationFactorOutOfState = 0.04; // Location factor for out of state
+
+    $rateHistoryFactorWithHistory = 0.01; // Rate history factor with history
+    $rateHistoryFactorWithoutHistory = 0; // Rate history factor without history
+
+    $gallonsRequestedFactorMoreThan1000 = 0.02; // Gallons requested factor for more than 1000 gallons
+    $gallonsRequestedFactorLessThan1000 = 0.03; // Gallons requested factor for less than 1000 gallons
+    $companyProfitFactor = 0.10; // Company profit factor
+
+    // Get input data from POST request
+    //$data = json_decode(file_get_contents('php://input'), true);
+    //$gallonsRequested = $data['gallonsRequested'];
+    //$state = $data['state'];
+    $hasRateHistory = false; // Assume no rate history initially
+    //$email = $client->getEmail();
+
+    //TESTING TESTING TESTING DUMMY VALUES
+
+    $gallonsRequested = 2000;
+    $state = "Texas";
+    $email = $client->getEmail();
+
+
+    // Check if client has rate history 
+    $stmt = $conn->prepare("SELECT COUNT(*) FROM fuelquote WHERE email = ?");
+    $stmt->bind_param("s", $email); // TESTING
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $count = $result->fetch_row()[0]; // Fetch the count value
+
+    if ($count > 0)
+    {
+        $hasRateHistory = true;
+    }
+
+    // Calculate margin
+    $locationFactor = $state === 'Texas' ? $locationFactorTexas : $locationFactorOutOfState;
+    $rateHistoryFactor = $hasRateHistory ? $rateHistoryFactorWithHistory : $rateHistoryFactorWithoutHistory;
+    $gallonsRequestedFactor = $gallonsRequested > 1000 ? $gallonsRequestedFactorMoreThan1000 : $gallonsRequestedFactorLessThan1000;
+    $margin = ($locationFactor - $rateHistoryFactor + $gallonsRequestedFactor + $companyProfitFactor) * $currentPricePerGallon;
+
+    // Calculate suggested price per gallon and total amount due
+    $suggestedPricePerGallon = $currentPricePerGallon + $margin;
+    $totalAmountDue = $gallonsRequested * $suggestedPricePerGallon;
+
+    // Prepare response data
+    $response = array(
+        'suggestedPrice' => number_format($suggestedPricePerGallon, 3),
+        'totalAmount' => number_format($totalAmountDue, 2)
+    );
+
+    // Send response as JSON
+    header('Content-Type: application/json');
+    echo json_encode($response);
+
+
+    ?>
